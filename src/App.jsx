7b693f76482a9f91fcd7a9c6e46548cb64104fe9
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-06-10 12:29:08 +0300

    Refactor App component: wrap return statement in a fragment for cleaner structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ function App() {
   const [currentPage, setCurrentPage] = useState(pages.home);
 
   return (
-    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+    <>
       <Navbar setCurrentPage={setCurrentPage} pages={pages} />
-      {currentPage === pages.home && <MainPage />}
-      {currentPage === pages.profile && <ProfilePage />}
-    </div>
+      <div className="max-w-screen-xl mx-auto p-4 sm:p-6 lg:px-8 text-center">
+        {currentPage === pages.home && <MainPage />}
+        {currentPage === pages.profile && <ProfilePage />}
+      </div>
+    </>
   );
 }
 export default App;
