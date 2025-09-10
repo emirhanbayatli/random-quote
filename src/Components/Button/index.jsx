6f4694be52184a2416aa23917937ea56cbd975c8
Emirhan Bayatlı <74186849+emirhanbayatli@users.noreply.github.com>
2025-09-10 14:48:31 +0300

    Update src/Components/Button/index.jsx
    Co-authored-by: Anna S. Atalay <annaatalay@hotmail.com>

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -2,7 +2,6 @@ import "./styles.css";
 
 export const Button = ({ label, handleOnClick, className }) => {
   return (
-    <>
       <button className={className} onClick={handleOnClick}>
         {label}
       </button>
