636487940edac8653737a960edad9138250532bb
emirhan.bayatli <emirhan.bayatli99@icloud.com>
2025-09-10 16:50:59 +0300

    Unused rows removed

--- src/components/QuoteCard/index.tsx
+++ src/components/QuoteCard/index.tsx
@@ -6,17 +6,6 @@ import { QuoteCardProps } from "../../types";
 import { Button } from "../Button";
 
 export const QuoteCard = ({ quote, author, likedBy }: QuoteCardProps) => {
-  // const dispatch = useQuotesDispatchContext();
-
-  // function handleClick() {
-  //   if (dispatch) {
-  //     dispatch({
-  //       type: QuotesActionType.LIKE_QUOTE,
-  //       payload: { quote: quote },
-  //     });
-  //   }
-  // }
-
   return (
     <>
       <section className="bg-slate-100 text-slate-800 dark:bg-slate-700 dark:text-slate-100 max-w-2xl my-5 mx-auto rounded-md p-10 text-lg">
@@ -24,7 +13,6 @@ export const QuoteCard = ({ quote, author, likedBy }: QuoteCardProps) => {
         <p>{author}</p>
         <p>{likedBy}</p>
       </section>
-      {/* <Button label="Like" handleOnClick={handleClick} /> */}
     </>
   );
 };
